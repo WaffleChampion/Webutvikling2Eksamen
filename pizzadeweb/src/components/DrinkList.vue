4bb196daf4bfaa42ba53ad072655031ed799b0df
Didesp <42587765+Didesp@users.noreply.github.com>
2020-04-13 15:48:08 +0200

    Drinks

---
+++ pizzadeweb/src/components/DrinkList.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <v-row>
+            <v-col cols="12" sm= "6" ig="4" v-for="drink in drinks" :key="drink.id">
+                <DrinkItem 
+                :id="drink.id"
+                :type="drink.type"
+                :price="drink.price"/>
+                
+                >
+
+            </v-col>
+
+        </v-row>
+</div>
+</template>
+<script>
+export default {
+    name: "DrinkList",
+    data(){
+        return{
+            drinks:[{id:1, type: "Coca Cola", price: 37}]
+        },
+        created()
+            axios.get("https://localhost:5001/Meny")
+            .then( result =>{
+                this.drinks = result.data;
+            }
+
+
+            )
+        },
+        components: {
+            DrinkItem
+        }
+
+
+    }
+
+</script>
