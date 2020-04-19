bbe1290a7cdbb56ed5b03b4234baad2d31095739
RedNoseCode <42588675+RedNoseCode@users.noreply.github.com>
2020-04-19 16:54:43 +0200

    changes to dessert.cs

--- pizzadeweb/src/components/DessertList.vue
+++ pizzadeweb/src/components/DessertList.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
     <v-row>
-        <v-col cols="12" sm="6" lg="4" v-for="dessert in dessertes" :key"dessert.id">
+        <v-col cols="12" sm="6" lg="4" v-for="dessert in dessertes" :key="dessert.id">
             <dessertItem
             :id="dessert.id"
             :type="dessert.dessertType"
@@ -29,7 +29,7 @@ export default {
     },
     created(){
         axios.get("https://localhost:5001/meny")
-        .then( result = {
+        .then( result => {
             this.dessertes = result.data;
         } )
     },
