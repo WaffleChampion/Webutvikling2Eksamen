4243dc13340348c78069febc0bc406a496474d4b
WaffleChampion <bendik1234@hotmail.no>
2020-04-23 14:24:59 +0200

    creating drinkform

---
+++ pizzadeweb/src/components/DrinkForm.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <v-row>
+            <v-col cols="12" sm="6" lg="4" class="mx-auto">
+                <v-text-field v-model="newDrink.drinkType" label="Navn på rett"></v-text-field>
+
+                <v-text-field v-model.number="newDrink.price" type="number" label="legg til pris"></v-text-field>
+                <v-file-input v-model="file" show-size></v-file-input>
+                <v-btn @click="postDish">Lagre ny rett</v-btn>
+            </v-col>
+        </v-row>
+    </div>
+</template>
+<script>
+import axios from 'axios'
+export default {
+    name: "DishForm",
+    data(){
+        return {
+            newDrink: { drinkType: "",  imageSrc: "" },
+            file: null
+        }
+    },
+    methods: {
+        postDish(){
+            this.newDrink.imageSrc = this.newDrink.dishType+".jpg";
+
+            let data = new FormData();
+            data.append("file", this.file);
+
+            axios.post("https://localhost:5001/Drink", this.newDrink).then( result => {
+                    console.log( result.data );
+
+                    axios({
+                        method: "POST",
+                        url: "https://localhost:5001/menyadmin/uploadimage",
+                        data: data,
+                        config: { headers: { 'Content-Type': 'multipart/form-data' } }       
+                    })
+
+                } )
+        }
+    }
+}
+</script>
