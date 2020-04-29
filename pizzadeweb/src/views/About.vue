f6526097c3b7638a6b3917babc62b141f072c9ee
WaffleChampion <bendik1234@hotmail.no>
2020-04-29 20:25:27 +0200

    Update About.vue

--- pizzadeweb/src/views/About.vue
+++ pizzadeweb/src/views/About.vue
@@ -9,26 +9,6 @@
        </v-col>
     </v-row>  
     </v-container>
-    <!--
-    <h1>PizzaDeWeb</h1>
-    <h2> Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
-      Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient 
-      montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, se
-      m. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet 
-      nec, vulputate eget, arcu. In enim justo, rhoncus ut,
-    </h2>'
-    <h2>
-      Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient 
-      montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, se
-      m. Nulla consequat massa quis enim.
-
-    </h2>
-
-    <h2>
-      Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient 
-      montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, se
-      m. Nulla consequat massa quis enim.
-    </h2>-->
     <ul>
       <li v-for="Text in texts" :key="Text.id">
           {{Text.info}}
