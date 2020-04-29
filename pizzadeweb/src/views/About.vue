27a5971df8e9a8db54aa75e23c993a65021b3ff7
RedNoseCode <42588675+RedNoseCode@users.noreply.github.com>
2020-04-29 20:23:42 +0200

    changes to about and store

--- pizzadeweb/src/views/About.vue
+++ pizzadeweb/src/views/About.vue
@@ -1,9 +1,14 @@
 <template>
 
 
-
-  
-  <div class="about">
+<div class="about">
+    <v-container>
+    <v-row justify="space-around">
+      <v-col cols="5">
+        <v-img src="https://images.squarespace-cdn.com/content/v1/5d9df6a0e4618a005f10d9ae/1573740806427-NOSL3CVM08PJ5GC6A4DA/ke17ZwdGBToddI8pDm48kLkXF2pIyv_F2eUT9F60jBl7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0iyqMbMesKd95J-X4EagrgU9L3Sa3U8cogeb0tjXbfawd0urKshkc5MgdBeJmALQKw/code1IMG_2003.jpg" aspect-ratio="1.7"></v-img>
+       </v-col>
+    </v-row>  
+    </v-container>
     <!--
     <h1>PizzaDeWeb</h1>
     <h2> Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
@@ -45,6 +50,7 @@ export default {
     }
   }
 }
+
 
 </script>
 
