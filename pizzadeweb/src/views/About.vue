2df03c19be1ef6299b3633a8be0ca6b57254c3e6
WaffleChampion <bendik1234@hotmail.no>
2020-04-29 19:00:32 +0200

    Update About.vue

--- pizzadeweb/src/views/About.vue
+++ pizzadeweb/src/views/About.vue
@@ -49,19 +49,13 @@ export default {
 </script>
 
 <style>
-#app {
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  background: url(https://image.freepik.com/free-photo/slice-delicious-pizza-with-ingredients-textured-wooden-background_23-2147926094.jpg) no-repeat center center fixed;
-  -webkit-background-size: cover;
-  -moz-background-size: cover;
-  -o-background-size: cover;
-  background-size: cover;
-}
+
 .about{
-  color: goldenrod;
+
+  background: url('../components/images/Frontpage/pizzabackground.jpg');
+  height: 100%;
+  /*width: 100%;*/
+  background-position: center;
+  background-size: cover;
 }
 </style>
