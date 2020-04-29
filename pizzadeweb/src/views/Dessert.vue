86b1ee702675ba6b40fd6db174d51946078c3bcc
WaffleChampion <bendik1234@hotmail.no>
2020-04-29 19:41:29 +0200

    fixes v2

--- pizzadeweb/src/views/Dessert.vue
+++ pizzadeweb/src/views/Dessert.vue
@@ -1,25 +1,10 @@
 <template>
-<<<<<<< Updated upstream
-    <div>
-              <div>
-            <router-link to="/">Home</router-link> |
-            <router-link to="/meny">Menu</router-link> |
-            <router-link to="/about">About</router-link> |
-      </div>
-        <div>
+    <div class="background">
+        <div id="nav">
             <router-link to="/dishes">Retter</router-link> |
             <router-link to="/drinks">Drikke</router-link> |
             <router-link to="/dessert">Dessert</router-link>|
-=======
-    <div class="background">
-        <div id="nav">
-            <router-link to="/dishes"><v-btn>Retter</v-btn></router-link> 
-           
-            <router-link to="/drinks"><v-btn>Drikke</v-btn></router-link> 
-            
-            <router-link to="/dessert"><v-btn>Dessert</v-btn></router-link>
->>>>>>> Stashed changes
-        </div>
+                    </div>
         <h1>Alle Desserter</h1>
         <DessertList />
 
@@ -34,13 +19,10 @@ export default {
         DessertList
     }
 }
-<<<<<<< Updated upstream
-</script>
-=======
 </script>
 
 <style lang="scss">
-.background{
+.background a{
   background: url('../components/images/Frontpage/pizzabackground.jpg');
   height: 100%;
   /*width: 100%;*/
@@ -53,7 +35,5 @@ export default {
   text-decoration: none;
   list-style-type: none;
   text-align: center;
-  margin: 3px;
 }
 </style>
->>>>>>> Stashed changes
